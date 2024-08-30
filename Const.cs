6096c92571df87299b12f9243b35bf01b1fedb24
Tejas Paradkar <67867777+tejasparadkar@users.noreply.github.com>
2024-08-30 19:46:13 +0530

    Add files via upload

---
+++ Const.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            a = 20;
+            Console.WriteLine(a);
+            const int b = 30;
+            Console.WriteLine(b);
+        }
+    }
+}
